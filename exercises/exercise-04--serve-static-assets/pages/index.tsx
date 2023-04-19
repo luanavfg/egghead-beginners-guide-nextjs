ec7540fd0bd318e58b56b915a84e2258a64d9eb9
luanavfg <luanavefago1@gmail.com>
2023-04-19 09:11:19 -0300

    feat: exercise-04

--- exercises/exercise-04--serve-static-assets/pages/index.tsx
+++ exercises/exercise-04--serve-static-assets/pages/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
       <h2>Hello 👋</h2>
       <p>My name is Lazar Nikolov</p>
       {/* ✍️ add the "lazar.png" image here */}
+      <img src="/lazar.png" />
     </div>
   )
 }
