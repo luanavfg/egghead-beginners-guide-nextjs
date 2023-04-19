56dfa0064aa4af6e35e12cbd4f59bc621324709a
luanavfg <luanavefago1@gmail.com>
2023-04-19 08:20:44 -0300

    feat: exercise-02

--- exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
+++ exercises/exercise-02--create-and-navigate-between-pages/pages/index.tsx
@@ -1,10 +1,13 @@
 // ✍️ import the Link component from 'next/link'
+import Link from "next/link"
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home page!</h1>
       {/* ✍️ put a link to the About page */}
+      {/* <a href="/about">About</a> */}
+      <Link href="/about">About</Link>
     </div>
   )
 }
