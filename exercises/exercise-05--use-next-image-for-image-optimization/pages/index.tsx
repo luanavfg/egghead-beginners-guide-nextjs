6d1e5a16e620856881092541982e0ea2d920f51a
luanavfg <luanavefago1@gmail.com>
2023-04-19 09:20:03 -0300

    feat: exercise-05 Next Image

--- exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
+++ exercises/exercise-05--use-next-image-for-image-optimization/pages/index.tsx
@@ -1,11 +1,20 @@
 // ✍️ import the Image component from 'next/image'
 
+import Image from "next/image"
+
 // ✍️ import "lazar.png" statically
 
+import lazar from '../lazar.png'
+
+const catImage = "https://www.petz.com.br/blog/wp-content/uploads/2021/11/enxoval-para-gato-Copia.jpg"
+
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
+  return (
+    <>
+      <Image src={lazar} alt="Lazar" />
+      <Image src={catImage} alt="cat" width={750} height={450}/>
+    </>
+  )
 }
 
 export default Home
